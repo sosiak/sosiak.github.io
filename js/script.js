e635edc6b87743314d9fd9c7ea8e1292eeecadfd
Slawomir Osiak <slawek-osiak@o2.pl>
2017-07-05 12:08:31 +0200

    correct background

--- js/script.js
+++ js/script.js
@@ -1,222 +1,211 @@
 $(document).ready(function() {
-	$("#myPhoto").hover(
-        function()
-        {
-            $(this).attr("src", "http://i67.tinypic.com/zy8f5.gif");
-        },
-        function()
-        {
-            $(this).attr("src", "http://i64.tinypic.com/2evwb5k.jpg");
-        }                         
-    );  
-	
 	 $(".red").click(function() {
 	 	$(".navbar").css({"background": "#c0392b"});
-	 	$(".navbar li a").css({"color": "#FF9287"});
+	 	$(".navbar li a").css({"color": "#FFDAD6"});
 	 	$(".navbar li a").hover(function () {
-	 		$(this).css("background", "#FF9287");
+	 		$(this).css("background", "#FFDAD6");
 	 		$(this).css("color", "#c0392b");}, function() {
 	 		$(this).css("background", "transparent");
-	 		$(this).css("color", "#FF9287");
+	 		$(this).css("color", "#FFDAD6");
 	 		});
-	 	$(".navbar-toggle").css({"border": "solid 1px #FF9287"});
-	 	$(".icon-bar").css({"background": "#FF9287"});
-	 	$(".navbar .navbar-brand .surname").css({"color": "#FF9287"});
-	 	$(".color").css({"background": "#FF9287"});
+	 	$(".navbar-toggle").css({"border": "solid 1px #FFDAD6"});
+	 	$(".icon-bar").css({"background": "#FFDAD6"});
+	 	$(".navbar .navbar-brand .surname").css({"color": "#FFDAD6"});
+	 	$(".color").css({"background": "#FFDAD6"});
 	 	$(".color h2").css({"color": "#c0392b"});
-	 	$(".about-me").css({"background": "#FF9287"});
+	 	$(".about-me").css({"background": "#FFDAD6"});
 	 	$(".about-me-paragraph p").css({"color": "#c0392b"});
 	 	$(".skills").css({"background": "#c0392b"});
-	 	$(".skills .row").css({"border-color": "#FF9287"});
-	 	$(".skills-head h1").css({"background": "#c0392b", "color": "#FF9287"});
-	 	$(".overlay").css({"background": "#c0392b", "border-color": "#FF9287"});
-	 	$(".portfolio").css({"background": "#FF9287"});
+	 	$(".skills .row").css({"border-color": "#FFDAD6"});
+	 	$(".skills-head h1").css({"background": "#c0392b", "color": "#FFDAD6"});
+	 	$(".overlay").css({"background": "#c0392b", "border-color": "#FFDAD6"});
+	 	$(".portfolio").css({"background": "#FFDAD6"});
 	 	$(".portfolio .row").css({"border-color": "#c0392b"});
-	 	$(".portfolio-head h1").css({"background": "#FF9287", "color": "#c0392b"});
-	 	$(".portfolio-image").css({"border-color": "#FF9287"});
-	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#FF9287"});
+	 	$(".portfolio-head h1").css({"background": "#FFDAD6", "color": "#c0392b"});
+	 	$(".portfolio-image").css({"border-color": "#FFDAD6"});
+	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#FFDAD6"});
 	 	$(".overlay-icon a").hover(function() {
 	 		$(this).css("background", "#9B2114");
-	 		$(this).css("color", "#c0392b");}, function() {
-	 		$(this).css("background", "#FF9287");
+	 		$(this).css("color", "#FFDAD6");}, function() {
+	 		$(this).css("background", "#FFDAD6");
 	 		$(this).css("color", "#9B2114");
 	 	});
 	 	$(".contact").css({"background": "#c0392b"});
-	 	$(".contact-border").css({"border-color": "#FF9287"});
-	 	$(".contact-head h1").css({"background": "#c0392b", "color": "#FF9287"});
-	 	$(".answer").css({"color": "#FF9287"});
-	 	$(".answer a").css({"color": "#FF9287"});
+	 	$(".contact-border").css({"border-color": "#FFDAD6"});
+	 	$(".contact-head h1").css({"background": "#c0392b", "color": "#FFDAD6"});
+	 	$(".answer").css({"color": "#FFDAD6"});
+	 	$(".answer a").css({"color": "#FFDAD6"});
 	 	$(".answer a").hover(function() {
 	 		$(this).css("color", "#720B00");}, function() {
-	 		$(this).css("color", "#FF9287");
+	 		$(this).css("color", "#FFDAD6");
 	 	});
 	 });
 
 	 $(".green").click(function() {
 	 	$(".navbar").css({"background": "#1D8013"});
-	 	$(".navbar li a").css({"color": "#34D534"});
+	 	$(".navbar li a").css({"color": "#94ED8B"});
 	 	$(".navbar li a").hover(function () {
-	 		$(this).css("background", "#34D534");
+	 		$(this).css("background", "#94ED8B");
 	 		$(this).css("color", "#1D8013");}, function() {
 	 		$(this).css("background", "transparent");
-	 		$(this).css("color", "#34D534");
+	 		$(this).css("color", "#94ED8B");
 	 		});
-	 	$(".navbar-toggle").css({"border": "solid 1px #34D534"});
-	 	$(".icon-bar").css({"background": "#34D534"});
-	 	$(".navbar .navbar-brand .surname").css({"color": "#34D534"});
-	 	$(".color").css({"background": "#34D534"});
+	 	$(".navbar-toggle").css({"border": "solid 1px #94ED8B"});
+	 	$(".icon-bar").css({"background": "#94ED8B"});
+	 	$(".navbar .navbar-brand .surname").css({"color": "#94ED8B"});
+	 	$(".color").css({"background": "#94ED8B"});
 	 	$(".color h2").css({"color": "#1D8013"});
-	 	$(".about-me").css({"background": "#34D534"});
+	 	$(".about-me").css({"background": "#94ED8B"});
 	 	$(".about-me-paragraph p").css({"color": "#1D8013"});
 	 	$(".skills").css({"background": "#1D8013"});
-	 	$(".skills .row").css({"border-color": "#34D534"});
-	 	$(".skills-head h1").css({"background": "#1D8013", "color": "#34D534"});
-	 	$(".overlay").css({"background": "#1D8013", "border-color": "#34D534"});
-	 	$(".portfolio").css({"background": "#34D534"});
+	 	$(".skills .row").css({"border-color": "#94ED8B"});
+	 	$(".skills-head h1").css({"background": "#1D8013", "color": "#94ED8B"});
+	 	$(".overlay").css({"background": "#1D8013", "border-color": "#94ED8B"});
+	 	$(".portfolio").css({"background": "#94ED8B"});
 	 	$(".portfolio .row").css({"border-color": "#1D8013"});
-	 	$(".portfolio-head h1").css({"background": "#34D534", "color": "#1D8013"});
-	 	$(".portfolio-image").css({"border-color": "#34D534"});
-	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#34D534"});
+	 	$(".portfolio-head h1").css({"background": "#94ED8B", "color": "#1D8013"});
+	 	$(".portfolio-image").css({"border-color": "#94ED8B"});
+	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#94ED8B"});
 	 	$(".overlay-icon a").hover(function() {
 	 		$(this).css("background", "#005500");
-	 		$(this).css("color", "#34D534");}, function() {
-	 		$(this).css("background", "#34D534");
+	 		$(this).css("color", "#94ED8B");}, function() {
+	 		$(this).css("background", "#94ED8B");
 	 		$(this).css("color", "#005500");
 	 	});
 	 	$(".contact").css({"background": "#1D8013"});
-	 	$(".contact-border").css({"border-color": "#34D534"});
-	 	$(".contact-head h1").css({"background": "#1D8013", "color": "#34D534"});
-	 	$(".answer").css({"color": "#34D534"});
-	 	$(".answer a").css({"color": "#34D534"});
+	 	$(".contact-border").css({"border-color": "#94ED8B"});
+	 	$(".contact-head h1").css({"background": "#1D8013", "color": "#94ED8B"});
+	 	$(".answer").css({"color": "#94ED8B"});
+	 	$(".answer a").css({"color": "#94ED8B"});
 	 	$(".answer a").hover(function() {
 	 		$(this).css("color", "#005500");}, function() {
-	 		$(this).css("color", "#34D534");
+	 		$(this).css("color", "#94ED8B");
 	 	});
 	 });
 
 	 $(".orange").click(function() {
 	 	$(".navbar").css({"background": "#e67e22"});
-	 	$(".navbar li a").css({"color": "#FFB878"});
+	 	$(".navbar li a").css({"color": "#FFF2E6"});
 	 	$(".navbar li a").hover(function () {
-	 		$(this).css("background", "#FFB878");
+	 		$(this).css("background", "#FFF2E6");
 	 		$(this).css("color", "#e67e22");}, function() {
 	 		$(this).css("background", "transparent");
-	 		$(this).css("color", "#FFB878");
+	 		$(this).css("color", "#FFF2E6");
 	 		});
-	 	$(".navbar-toggle").css({"border": "solid 1px #FFB878"});
-	 	$(".icon-bar").css({"background": "#FFB878"});
-	 	$(".navbar .navbar-brand .surname").css({"color": "#FFB878"});
-	 	$(".color").css({"background": "#FFB878"});
+	 	$(".navbar-toggle").css({"border": "solid 1px #FFF2E6"});
+	 	$(".icon-bar").css({"background": "#FFF2E6"});
+	 	$(".navbar .navbar-brand .surname").css({"color": "#FFF2E6"});
+	 	$(".color").css({"background": "#FFF2E6"});
 	 	$(".color h2").css({"color": "#e67e22"});
-	 	$(".about-me").css({"background": "#FFB878"});
+	 	$(".about-me").css({"background": "#FFF2E6"});
 	 	$(".about-me-paragraph p").css({"color": "#e67e22"});
 	 	$(".skills").css({"background": "#e67e22"});
-	 	$(".skills .row").css({"border-color": "#FFB878"});
-	 	$(".skills-head h1").css({"background": "#e67e22", "color": "#FFB878"});
-	 	$(".overlay").css({"background": "#e67e22", "border-color": "#FFB878"});
-	 	$(".portfolio").css({"background": "#FFB878"});
+	 	$(".skills .row").css({"border-color": "#FFF2E6"});
+	 	$(".skills-head h1").css({"background": "#e67e22", "color": "#FFF2E6"});
+	 	$(".overlay").css({"background": "#e67e22", "border-color": "#FFF2E6"});
+	 	$(".portfolio").css({"background": "#FFF2E6"});
 	 	$(".portfolio .row").css({"border-color": "#e67e22"});
-	 	$(".portfolio-head h1").css({"background": "#FFB878", "color": "#e67e22"});
-	 	$(".portfolio-image").css({"border-color": "#FFB878"});
-	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#FFB878"});
+	 	$(".portfolio-head h1").css({"background": "#FFF2E6", "color": "#e67e22"});
+	 	$(".portfolio-image").css({"border-color": "#FFF2E6"});
+	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#FFF2E6"});
 	 	$(".overlay-icon a").hover(function() {
 	 		$(this).css("background", "#924500");
-	 		$(this).css("color", "#FFB878");}, function() {
-	 		$(this).css("background", "#FFB878");
+	 		$(this).css("color", "#FFF2E6");}, function() {
+	 		$(this).css("background", "#FFF2E6");
 	 		$(this).css("color", "#924500");
 	 	});
 	 	$(".contact").css({"background": "#e67e22"});
-	 	$(".contact-border").css({"border-color": "#FFB878"});
-	 	$(".contact-head h1").css({"background": "#e67e22", "color": "#FFB878"});
-	 	$(".answer").css({"color": "#FFB878"});
-	 	$(".answer a").css({"color": "#FFB878"});
+	 	$(".contact-border").css({"border-color": "#FFF2E6"});
+	 	$(".contact-head h1").css({"background": "#e67e22", "color": "#FFF2E6"});
+	 	$(".answer").css({"color": "#FFF2E6"});
+	 	$(".answer a").css({"color": "#FFF2E6"});
 	 	$(".answer a").hover(function() {
 	 		$(this).css("color", "#924500");}, function() {
-	 		$(this).css("color", "#FFB878");
+	 		$(this).css("color", "#FFF2E6");
 	 	});
 	 });
 
 	 $(".pink").click(function() {
-	 	$(".navbar").css({"background": "#8e44ad"});
-	 	$(".navbar li a").css({"color": "#CC9BE1"});
+	 	$(".navbar").css({"background": "#BF45A1"});
+	 	$(".navbar li a").css({"color": "#FAD8F1"});
 	 	$(".navbar li a").hover(function () {
-	 		$(this).css("background", "#CC9BE1");
-	 		$(this).css("color", "#8e44ad");}, function() {
+	 		$(this).css("background", "#FAD8F1");
+	 		$(this).css("color", "#BF45A1");}, function() {
 	 		$(this).css("background", "transparent");
-	 		$(this).css("color", "#CC9BE1");
+	 		$(this).css("color", "#FAD8F1");
 	 		});
-	 	$(".navbar-toggle").css({"border": "solid 1px #CC9BE1"});
-	 	$(".icon-bar").css({"background": "#CC9BE1"});
-	 	$(".navbar .navbar-brand .surname").css({"color": "#CC9BE1"});
-	 	$(".color").css({"background": "#CC9BE1"});
-	 	$(".color h2").css({"color": "#8e44ad"});
-	 	$(".about-me").css({"background": "#CC9BE1"});
-	 	$(".about-me-paragraph p").css({"color": "#8e44ad"});
-	 	$(".skills").css({"background": "#8e44ad"});
-	 	$(".skills .row").css({"border-color": "#CC9BE1"});
-	 	$(".skills-head h1").css({"background": "#8e44ad", "color": "#CC9BE1"});
-	 	$(".overlay").css({"background": "#8e44ad", "border-color": "#CC9BE1"});
-	 	$(".portfolio").css({"background": "#CC9BE1"});
-	 	$(".portfolio .row").css({"border-color": "#8e44ad"});
-	 	$(".portfolio-head h1").css({"background": "#CC9BE1", "color": "#8e44ad"});
-	 	$(".portfolio-image").css({"border-color": "#CC9BE1"});
-	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#CC9BE1"});
+	 	$(".navbar-toggle").css({"border": "solid 1px #FAD8F1"});
+	 	$(".icon-bar").css({"background": "#FAD8F1"});
+	 	$(".navbar .navbar-brand .surname").css({"color": "#FAD8F1"});
+	 	$(".color").css({"background": "#FAD8F1"});
+	 	$(".color h2").css({"color": "#BF45A1"});
+	 	$(".about-me").css({"background": "#FAD8F1"});
+	 	$(".about-me-paragraph p").css({"color": "#BF45A1"});
+	 	$(".skills").css({"background": "#BF45A1"});
+	 	$(".skills .row").css({"border-color": "#FAD8F1"});
+	 	$(".skills-head h1").css({"background": "#BF45A1", "color": "#FAD8F1"});
+	 	$(".overlay").css({"background": "#BF45A1", "border-color": "#FAD8F1"});
+	 	$(".portfolio").css({"background": "#FAD8F1"});
+	 	$(".portfolio .row").css({"border-color": "#BF45A1"});
+	 	$(".portfolio-head h1").css({"background": "#FAD8F1", "color": "#BF45A1"});
+	 	$(".portfolio-image").css({"border-color": "#FAD8F1"});
+	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#FAD8F1"});
 	 	$(".overlay-icon a").hover(function() {
 	 		$(this).css("background", "#190323");
-	 		$(this).css("color", "#CC9BE1");}, function() {
-	 		$(this).css("background", "#CC9BE1");
+	 		$(this).css("color", "#FAD8F1");}, function() {
+	 		$(this).css("background", "#FAD8F1");
 	 		$(this).css("color", "#190323");
 	 	});
-	 	$(".contact").css({"background": "#8e44ad"});
-	 	$(".contact-border").css({"border-color": "#CC9BE1"});
-	 	$(".contact-head h1").css({"background": "#8e44ad", "color": "#CC9BE1"});
-	 	$(".answer").css({"color": "#CC9BE1"});
-	 	$(".answer a").css({"color": "#CC9BE1"});
+	 	$(".contact").css({"background": "#BF45A1"});
+	 	$(".contact-border").css({"border-color": "#FAD8F1"});
+	 	$(".contact-head h1").css({"background": "#BF45A1", "color": "#FAD8F1"});
+	 	$(".answer").css({"color": "#FAD8F1"});
+	 	$(".answer a").css({"color": "#FAD8F1"});
 	 	$(".answer a").hover(function() {
 	 		$(this).css("color", "#190323");}, function() {
-	 		$(this).css("color", "#CC9BE1");
+	 		$(this).css("color", "#FAD8F1");
 	 	});
 	 });
 
 	 $(".blue").click(function() {
-	 	$(".navbar").css({"background": "#096EB0"});
-	 	$(".navbar li a").css({"color": "#72B0D9"});
+	 	$(".navbar").css({"background": "#0F4AB6"});
+	 	$(".navbar li a").css({"color": "#D9E6FF"});
 	 	$(".navbar li a").hover(function () {
-	 		$(this).css("background", "#72B0D9");
-	 		$(this).css("color", "#096EB0");}, function() {
+	 		$(this).css("background", "#D9E6FF");
+	 		$(this).css("color", "#0F4AB6");}, function() {
 	 		$(this).css("background", "transparent");
-	 		$(this).css("color", "#72B0D9");
+	 		$(this).css("color", "#D9E6FF");
 	 		});
-	 	$(".navbar-toggle").css({"border": "solid 1px #72B0D9"});
-	 	$(".icon-bar").css({"background": "#72B0D9"});
-	 	$(".navbar .navbar-brand .surname").css({"color": "#72B0D9"});
-	 	$(".color").css({"background": "#72B0D9"});
-	 	$(".color h2").css({"color": "#096EB0"});
-	 	$(".about-me").css({"background": "#72B0D9"});
-	 	$(".about-me-paragraph p").css({"color": "#096EB0"});
-	 	$(".skills").css({"background": "#096EB0"});
-	 	$(".skills .row").css({"border-color": "#72B0D9"});
-	 	$(".skills-head h1").css({"background": "#096EB0", "color": "#72B0D9"});
-	 	$(".overlay").css({"background": "#096EB0", "border-color": "#72B0D9"});
-	 	$(".portfolio").css({"background": "#72B0D9"});
-	 	$(".portfolio .row").css({"border-color": "#096EB0"});
-	 	$(".portfolio-head h1").css({"background": "#72B0D9", "color": "#096EB0"});
-	 	$(".portfolio-image").css({"border-color": "#72B0D9"});
-	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#72B0D9"});
+	 	$(".navbar-toggle").css({"border": "solid 1px #D9E6FF"});
+	 	$(".icon-bar").css({"background": "#D9E6FF"});
+	 	$(".navbar .navbar-brand .surname").css({"color": "#D9E6FF"});
+	 	$(".color").css({"background": "#D9E6FF"});
+	 	$(".color h2").css({"color": "#0F4AB6"});
+	 	$(".about-me").css({"background": "#D9E6FF"});
+	 	$(".about-me-paragraph p").css({"color": "#0F4AB6"});
+	 	$(".skills").css({"background": "#0F4AB6"});
+	 	$(".skills .row").css({"border-color": "#D9E6FF"});
+	 	$(".skills-head h1").css({"background": "#0F4AB6", "color": "#D9E6FF"});
+	 	$(".overlay").css({"background": "#0F4AB6", "border-color": "#D9E6FF"});
+	 	$(".portfolio").css({"background": "#D9E6FF"});
+	 	$(".portfolio .row").css({"border-color": "#0F4AB6"});
+	 	$(".portfolio-head h1").css({"background": "#D9E6FF", "color": "#0F4AB6"});
+	 	$(".portfolio-image").css({"border-color": "#D9E6FF"});
+	 	$(".overlay-icon a").css({"color": "#9B2114", "background": "#D9E6FF"});
 	 	$(".overlay-icon a").hover(function() {
 	 		$(this).css("background", "#190323");
-	 		$(this).css("color", "#72B0D9");}, function() {
-	 		$(this).css("background", "#72B0D9");
+	 		$(this).css("color", "#D9E6FF");}, function() {
+	 		$(this).css("background", "#D9E6FF");
 	 		$(this).css("color", "#190323");
 	 	});
-	 	$(".contact").css({"background": "#096EB0"});
-	 	$(".contact-border").css({"border-color": "#72B0D9"});
-	 	$(".contact-head h1").css({"background": "#096EB0", "color": "#72B0D9"});
-	 	$(".answer").css({"color": "#72B0D9"});
-	 	$(".answer a").css({"color": "#72B0D9"});
+	 	$(".contact").css({"background": "#0F4AB6"});
+	 	$(".contact-border").css({"border-color": "#D9E6FF"});
+	 	$(".contact-head h1").css({"background": "#0F4AB6", "color": "#D9E6FF"});
+	 	$(".answer").css({"color": "#D9E6FF"});
+	 	$(".answer a").css({"color": "#D9E6FF"});
 	 	$(".answer a").hover(function() {
 	 		$(this).css("color", "#190323");}, function() {
-	 		$(this).css("color", "#72B0D9");
+	 		$(this).css("color", "#D9E6FF");
 	 	});
 	 });
 
